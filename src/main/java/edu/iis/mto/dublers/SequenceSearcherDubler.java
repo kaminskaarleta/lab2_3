fbf1cc723c8793067acadabc8d54d613589d524b
Kamińska <180195@edu.p.lodz.pl>
2015-03-19 20:25:36 +0100

    Added dubler for SequenceSearcher class

---
+++ src/main/java/edu/iis/mto/dublers/SequenceSearcherDubler.java
@@ -0,0 +1,12 @@
+package edu.iis.mto.dublers;
+
+import edu.iis.mto.search.SearchResult;
+import edu.iis.mto.search.SequenceSearcher;
+
+public class SequenceSearcherDubler implements SequenceSearcher{
+
+	public SearchResult search(int key, int[] seq) {
+		return null;
+	}
+
+}
