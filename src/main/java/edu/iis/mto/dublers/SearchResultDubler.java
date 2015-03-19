8e84673741acfd31574d870c0541bb0f4379186e
Kamińska <180195@edu.p.lodz.pl>
2015-03-19 20:23:48 +0100

    Added dubler for SearchResult class

---
+++ src/main/java/edu/iis/mto/dublers/SearchResultDubler.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.dublers;
+
+import edu.iis.mto.search.SearchResult;
+
+public class SearchResultDubler implements SearchResult{
+
+	public boolean isFound() {
+		return false;
+	}
+
+	public int getPosition() {
+		return 0;
+	}
+
+}
